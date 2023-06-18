438aa3037510a174947fd386b94977bf380e3034
Sonika Gupta <133162380+Sonikaguptaa@users.noreply.github.com>
2023-06-18 13:29:14 -0600

    javaScript.js

--- DOM MENU LAB 2/javaScript.js
+++ DOM MENU LAB 2/javaScript.js
@@ -28,46 +28,42 @@ var menuLinks = [
   },
 ];
 //1.0
-// Select the <main> element
+
 let mainEl = document.querySelector("main");
 
-// Set the background color of the <main> element using a CSS variable
+
 mainEl.style.backgroundColor = "var(--main-bg)";
 
-// Set the content of the <main> element to a heading
+
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 
-// Add a CSS class to the <main> element to center its contents
 mainEl.classList.add("flex-ctr");
 
 //2.0
-// Select the element with id "top-menu"
+
 let topMenuE1 = document.querySelector("#top-menu");
 
-// Set the height of the "top-menu" element to 100%
+
 topMenuE1.style.height = "100%";
 
-// Set the background color of the "top-menu" element using a CSS variable
 topMenuE1.style.backgroundColor = "var(--top-menu-bg)";
 
-// Add a CSS class to the "top-menu" element to arrange its contents with flex layout
+
 topMenuE1.classList.add("flex-around");
 //3.0
-// Iterate through the menuLinks array
+
 for (let i = 0; i < menuLinks.length; i++) {
-  // Print the text of the current menu link
+  
   console.log(menuLinks[i].text);
 
-  // Create a new <a> element for the menu link
   let mlink = document.createElement("a");
 
-  // Set the innerHTML of the <a> element to the text of the menu link
+
   mlink.innerHTML = menuLinks[i].text;
 
-  // Set the "href" attribute of the <a> element to the href value of the menu link
   mlink.setAttribute("href", menuLinks[i].href);
 
-  // Append the menu link to the "top-menu" element
+  
   topMenuE1.appendChild(mlink);
 }
 //4.0
